eb1117fd78128b1deb1a72f4dbab562c94a919bd
NeilEntote <56211468+NeilEntote@users.noreply.github.com>
2021-02-13 17:37:23 -0500

    Add roomCheck function and 6 corner room splitter

--- geofencing.py
+++ geofencing.py
@@ -2,10 +2,30 @@ import json
 import os
 import time
 
+# Pass current location, and room data to determine if person is in a certain room
+def roomCheck(curLoc, roomData):
+    for XValue in roomData["cornersX"]:
+        #loc is greater than any single x value
+        if  curLoc[0] > XValue:
+            for XValue in roomData["cornersX"]:
+                #loc is less than any single x value (betweeen 2 X)
+                if  curLoc[0] < XValue:
+                    for YValue in roomData["cornersY"]:
+                        #loc is greater than any single y value
+                        if  curLoc[1] > YValue:
+                            for YValue in roomData["cornersY"]:
+                                #loc is less than any single y value (betweeen 2 Y)
+                                if  curLoc[1] < YValue:
+                                    return True
+                                else:
+                                    return False
+
 def checkLoc(curLoc, geofencingData, roomStatus):
     i = 0
-    for room in geofencingData:
-        i += 1
+    for room in geofencingData: 
+        i += 1 
+        # When using the roomCheck function, just pass the room you are in
+        # -> roomCheck(curLoc, geofencingData["room"+str(i)])
         if geofencingData["room"+str(i)]["type"] == "4 corner":
             for XValue in geofencingData["room"+str(i)]["cornersX"]:
                 #loc is greater than any single x value
@@ -21,17 +41,6 @@ def checkLoc(curLoc, geofencingData, roomStatus):
                                         if  curLoc[1] < YValue:
                                             roomStatus["room"+str(i)] = True
         else:
-            pass #@neil
-
-    return roomStatus
-
-
-path = os.path.dirname(__file__)
-f = open(path + '/sample.json', "r")
-geofencingData = json.load(f)
-f.close()
-
-        if room["type"] == "6 corner":
             # Generate 2 "rooms"
             room6Corners = {
                 "rectangle 1" : {
@@ -44,11 +53,24 @@ f.close()
                 }
             }
 
-            for i in range(4):
-                room6Corners["rectangle 1"]["cornersX"].append(room["cornersX"][i]) 
-                room6Corners["rectangle 1"]["cornersY"].append(room["cornersY"][i])
+            # Split 6 corner room 2 into 2, 4 corners (Includes 7th point)
+            # First rectangle
+            for j in range(4):
+                room6Corners["rectangle 1"]["cornersX"].append(geofencingData["room"+str(i)]["cornersX"][j]) 
+                room6Corners["rectangle 1"]["cornersY"].append(geofencingData["room"+str(i)]["cornersY"][j])
             
-            for i in range(4, 7):
-                room6Corners["rectangle 2"]["cornersX"].append(room["cornersX"][i]) 
-                room6Corners["rectangle 2"]["cornersY"].append(room["cornersY"][i])
-    f.close()
+            roomStatus["room"+str(i)] = roomCheck(curLoc, room6Corners["rectangle 1"])
+            
+            # Second rectangle
+            for j in range(4, 7):
+                room6Corners["rectangle 2"]["cornersX"].append(geofencingData["room"+str(i)]["cornersX"][j]) 
+                room6Corners["rectangle 2"]["cornersY"].append(geofencingData["room"+str(i)]["cornersX"][j])
+
+            roomStatus["room"+str(i)] = roomCheck(curLoc, room6Corners["rectangle 2"])
+
+    return roomStatus
+
+path = os.path.dirname(__file__)
+f = open(path + '/sample.json', "r")
+geofencingData = json.load(f)
+f.close()
