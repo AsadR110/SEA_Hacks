0351b86998b218efafb9ba062ccf6929bb025eea
AsadR110 <62395847+AsadR110@users.noreply.github.com>
2021-02-14 00:21:55 -0500

    Add files via upload

--- Webpage/testingWebpage.py
+++ Webpage/testingWebpage.py
@@ -3,19 +3,33 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def homepage():
     return render_template("roomWebpage.html")
 
-# @app.route("/")
-# def home():
-#     title = "Rooms"
-#     paragraph = ["Here's where I'm entering all the cool stuff", "This is the first part", "This is the second part"]
-#     return render_template("testingLogic.html", title = title, paragraph = paragraph)
 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route("/page2")
+def home():
+    title = "Rooms"
+    paragraph = ["Here's where I'm entering all the cool stuff", "This is the first part", "This is the second part"]
+    return render_template("testingLogic.html", title = title, paragraph = paragraph)
+
+
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+# @app.route("/<usr>")
+# def user(usr):
+#     return  f"<h1>{usr}</h1>"
+
+
+
+
 
 @app.route(("/admin"))
 def admin():
